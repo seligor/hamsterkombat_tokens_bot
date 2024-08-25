73b7d6219b1e095b2a161c87ce3fa62a8b92d36c
Igor Baluev <s-el-igor@mail.ru>
2024-08-25 20:50:56 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,17 +2,3 @@ python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
 deactivate
-
-chmod a+x START.sh
-
-name=`pwd | grep -o '[^/]*$'`
-
-echo "#/bin/sh" > start.sh
-echo "screen -S $name ./START.sh" >> start.sh
-
-chmod a+x start.sh
-
-echo "#/bin/sh" > use.sh
-echo "screen -r $name" >> use.sh
-
-chmod a+x use.sh
